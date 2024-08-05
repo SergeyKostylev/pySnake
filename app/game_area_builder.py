5d85b7aa181e2b2f98bb32f5478e4f2f723028c5
s.kostyliev <s.kostylev@icecat.biz>
2024-08-05 16:44:26 +0300

    fix

--- app/game_area_builder.py
+++ app/game_area_builder.py
@@ -20,18 +20,30 @@ def build_game_area() -> GameArea:
 def build_snake(snake_field: SnakeField) -> Snake:
     snake_field_center_position = snake_field.get_center_point()
 
-    return Snake(position=snake_field_center_position)
+    position = (
+        round_to_point_size(snake_field_center_position[0]),
+        round_to_point_size(snake_field_center_position[1])
+    )
+
+    return Snake(position=position)
 
 
 def build_snake_field() -> SnakeField:
     screen_size = conf.SCREEN_SIZE
     field_size = conf.SNAKE_FIELD_SIZE
-    # x = (screen_size[0] - field_size[0]) // 2 TODO: for test only
+    # x = round_to_point_size((screen_size[0] - field_size[0]) // 2) TODO: for test only
     x = 20  # temporary  solution
-    y = (screen_size[1] - field_size[1]) // 2
+    y = round_to_point_size((screen_size[1] - field_size[1]) // 2)
 
     return SnakeField(conf.SNAKE_FIELD_SIZE[0], conf.SNAKE_FIELD_SIZE[1], x, y)
 
 
 def build_target(point: tuple[int, int]) -> Target:
-    return Target(point[0], point[1])
+    return Target(
+        round_to_point_size(point[0]),
+        round_to_point_size(point[1])
+    )
+
+
+def round_to_point_size(value):
+    return (value // conf.FIELD_POINT_SIZE) * conf.FIELD_POINT_SIZE
