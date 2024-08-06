76d2518f49265ecd667d6f888c57d4f7a31ce0f6
s.kostyliev <s.kostylev@icecat.biz>
2024-08-06 15:03:45 +0300

    game over window

--- app/models/rendered/game_over_window.py
+++ app/models/rendered/game_over_window.py
@@ -11,10 +11,8 @@ from app.models.rendered.base_rendered_model import BaseRenderedModel
 
 class GameOverWindow(BaseRenderedModel):
 
-    __text = "GAME OVER"
     def __init__(self):
         self.__window = self.__build()
-
 
     def __build(self):
         width, height = conf.DIALOG_WINDOW_SIZE
@@ -25,6 +23,20 @@ class GameOverWindow(BaseRenderedModel):
 
     def render(self, screen: pygame.Surface):
         pygame.draw.rect(screen, conf.DIALOG_WINDOW_COLOR, self.__window)
+
+        texts = [
+            "GAME OVER",
+            "Press N to quit",
+            "Press Y to start a new game"
+        ]
+
+        font = pygame.font.SysFont('Arial', conf.INFO_BAR_TEXT_SIZE)
+        y_shift = 0
+        for text in texts:
+            surface = font.render(text, True, conf.DIALOG_WINDOW_TEXT_COLOR)
+            x_center = self.__window.x + (self.__window.width // 2) - (surface.get_rect().width // 2)
+            screen.blit(surface, (x_center, self.__window.y + y_shift))
+            y_shift += conf.INFO_BAR_TEXT_SIZE
 
     def start_new_game(self) -> bool:
         while True:
